99fe6974e0342094ab910f68422728fb7bbfae94
dhruvpant116 <dhruvpant116@gmail.com>
2022-11-14 02:13:08 +0530

    Added Code

---
+++ login.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Admin | Loan Management System</title>
+ 	
+
+<?php include('./header.php'); ?>
+<?php include('./db_connect.php'); ?>
+<?php 
+session_start();
+if(isset($_SESSION['login_id']))
+header("location:index.php?page=home");
+
+?>
+
+</head>
+<style>
+	body{
+		width: 100%;
+	    height: calc(100%);
+	    /*background: #007bff;*/
+		background-color: rgb(196, 232, 244);
+	}
+	main#main{
+		width:100%;
+		height: calc(100%);
+		
+	}
+	#login-right{
+		position: absolute;
+		right:400px;
+		width:34%;
+		height: 80%;
+		display: flex;
+		align-items: center;
+		background: #fcfdd2;
+		margin: 70px;
+		opacity: 90%;
+
+
+	}
+	#login-left{
+		opacity: 60%;
+		position: absolute;
+		left:0;
+		width:100%;
+		height: calc(100%);
+		background:#59b6ec61;
+		display: flex;
+		align-items: center;
+		background: url(assets/img/img1.png);
+	    background-repeat: no-repeat;
+	    background-size: cover;
+	}
+	#login-right .card{
+		margin: auto;
+		z-index: 1;
+		background: #fcfdd2;
+		border: none;
+		
+	}
+	.logo {
+    margin: auto;
+    font-size: 8rem;
+	/* margin-right: 100px ; */
+    /* background: white; */
+    padding: .5em 0.4em;
+    border-radius: 50% 50%;
+    color: #000000b3;
+    z-index: 10;
+}
+div#login-right::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: calc(100%);
+    height: calc(100%);
+    background: #fcfdd2;
+}
+
+label{
+	color: #63bdff;
+	font-weight: 700;
+}
+
+input{
+	border: 2px solid white;
+	border-radius: 15px;
+}
+
+</style>
+
+<body>
+
+
+  <main id="main" class=" bg-dark">
+  		<div id="login-left">
+  			
+  		</div>
+
+  		<div id="login-right">
+  			<div class="card col-md-8">
+  				<div class="card-body">
+  						<div class="logo">
+			  				<span class="fa fa-money-bill-wave"></span>
+			  			</div>
+  					<form id="login-form" >
+  						<div class="form-group">
+  							<label for="username" class="control-label">Username</label>
+  							<input type="text" id="username" name="username" class="form-control">
+  						</div>
+  						<div class="form-group">
+  							<label for="password" class="control-label">Password</label>
+  							<input type="password" id="password" name="password" class="form-control">
+  						</div>
+  						<center><button class="btn-lg btn-block btn-wave col-md-4 btn-primary" style="background: #77eeaa; border:none; border-radius:20px;">Login</button></center>
+  					</form>
+  				</div>
+  			</div>
+  		</div>
+   
+
+  </main>
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+
+</body>
+<script>
+	$('#login-form').submit(function(e){
+		e.preventDefault()
+		$('#login-form button[type="button"]').attr('disabled',true).html('Logging in...');
+		if($(this).find('.alert-danger').length > 0 )
+			$(this).find('.alert-danger').remove();
+		$.ajax({
+			url:'ajax.php?action=login',
+			method:'POST',
+			data:$(this).serialize(),
+			error:err=>{
+				console.log(err)
+		$('#login-form button[type="button"]').removeAttr('disabled').html('Login');
+
+			},
+			success:function(resp){
+				if(resp == 1){
+					location.href ='index.php?page=home';
+				}else if(resp == 2){
+					location.href ='voting.php';
+				}else{
+					$('#login-form').prepend('<div class="alert alert-danger">Username or password is incorrect.</div>')
+					$('#login-form button[type="button"]').removeAttr('disabled').html('Login');
+				}
+			}
+		})
+	})
+</script>	
+</html>
